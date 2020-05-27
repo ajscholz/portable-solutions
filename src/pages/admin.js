09c49dd9e88f093ec43fe89590347373c1bf758a
Andrew Scholz <yoitsscholzy@gmail.com>
2020-05-27 16:59:41 -0400

    add buttons to admin page

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -2,8 +2,79 @@ import React, { useRef, useState } from "react"
 import { graphql } from "gatsby"
 import { Popover, PopoverBody } from "reactstrap"
 import PasswordModal from "../components/PasswordModal"
+import { Link } from "gatsby"
 
 const pStyle = { marginBottom: "0px" }
+
+const Admin = ({ data }) => {
+  const [login, setLogin] = useState(true)
+
+  return login ? (
+    <PasswordModal
+      setShowCrates={() => {}}
+      showLoginModal={login}
+      setShowLoginModal={setLogin}
+      headerText="Admin Page Login"
+      bodyText=""
+    />
+  ) : (
+    <>
+      <div className="w-100 d-flex justify-content-between p-5">
+        <div className="mr-5">
+          <h1>PS Admin Page</h1>
+          <h3>Crate Page Passwords</h3>
+
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(2, max-content)",
+              columnGap: "2rem",
+            }}
+          >
+            <h5 style={{ textDecoration: "underline", marginBottom: "0" }}>
+              Crate Name
+            </h5>
+            <h5 style={{ textDecoration: "underline", marginBottom: "0" }}>
+              Password
+            </h5>
+            {data.crates.all.map(crate => (
+              <PasswordRow key={crate.password} crate={crate} />
+            ))}
+          </div>
+        </div>
+        <div className="ml-5 d-flex flex-column justify-content-start flex-wrap">
+          <Link className="btn btn-lg" to="/rta">
+            RTA Crates
+          </Link>
+          <Link className="btn btn-lg" to="/diy">
+            DIY Crates
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export const query = graphql`
+  {
+    crates: allContentfulCrate(
+      filter: {
+        page_section: {
+          elemMatch: { title: { in: ["RTA Crate List", "DIY Guides List"] } }
+        }
+      }
+    ) {
+      all: nodes {
+        name
+        fields {
+          password
+        }
+      }
+    }
+  }
+`
+
+export default Admin
 
 const PasswordRow = ({ crate }) => {
   const [copied, setCopied] = useState(false)
@@ -55,63 +126,3 @@ const PasswordRow = ({ crate }) => {
     </>
   )
 }
-
-const Admin = ({ data }) => {
-  const [login, setLogin] = useState(true)
-
-  return login ? (
-    <PasswordModal
-      setShowCrates={() => {}}
-      showLoginModal={login}
-      setShowLoginModal={setLogin}
-      headerText="Admin Page Login"
-      bodyText=""
-    />
-  ) : (
-    <>
-      <div style={{ padding: "2rem" }}>
-        <h1>PS Admin Page</h1>
-        <h3>Crate Page Passwords</h3>
-
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(2, max-content)",
-            columnGap: "2rem",
-          }}
-        >
-          <h5 style={{ textDecoration: "underline", marginBottom: "0" }}>
-            Crate Name
-          </h5>
-          <h5 style={{ textDecoration: "underline", marginBottom: "0" }}>
-            Password
-          </h5>
-          {data.crates.all.map(crate => (
-            <PasswordRow key={crate.password} crate={crate} />
-          ))}
-        </div>
-      </div>
-    </>
-  )
-}
-
-export const query = graphql`
-  {
-    crates: allContentfulCrate(
-      filter: {
-        page_section: {
-          elemMatch: { title: { in: ["RTA Crate List", "DIY Guides List"] } }
-        }
-      }
-    ) {
-      all: nodes {
-        name
-        fields {
-          password
-        }
-      }
-    }
-  }
-`
-
-export default Admin
